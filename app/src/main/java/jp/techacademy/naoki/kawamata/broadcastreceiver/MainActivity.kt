0a96748f64dca31f7b3c3a3704bc6dfba1486ec4
kawatech <kawatech@gol.com>
2020-01-14 20:44:42 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/naoki/kawamata/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,15 @@
+package jp.techacademy.naoki.kawamata.broadcastreceiver
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import jp.techacademy.taro.kirameki.broadcastreceiver.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
